ac70b1d8d0436084864e87b27c18a2a583c5d3f0
Kishin520 <nicokishins@hotmail.com>
2023-11-16 04:42:11 -0300

    cambios a la app de forma visual

--- src/App.vue
+++ src/App.vue
@@ -36,53 +36,56 @@ export default {
     <link rel="stylesheet" href="./assets/styles.css">
   </head>
   <body class="bg-[#872341]">
-    <section class="flex-col">
-        <h1  class ="text-4xl text-center" v-if="profile">Perfil Profesional: {{ profile.name }} {{ profile.lastname }}</h1>
-        <p class="text-2xl italic text-center"> {{ profile.summary }}</p>
-        <h2 class="text-2xl text-center">Datos Personales</h2>
-        <div class="flex flex-nowrap">
-          <p class="flex-1 bg-[#22092C] text-center text-[aliceblue] m-2.5 p-[15px] rounded-lg">Edad: {{profile.age}}</p>
-          <p class="flex-1 bg-[#22092C] text-center text-[aliceblue] m-2.5 p-[15px] rounded-lg">Ciudad de origen: {{ profile.city }}</p>
-          <p class="flex-1 bg-[#22092C] text-center text-[aliceblue] m-2.5 p-[15px] rounded-lg">Correo: {{ profile.email }}</p>
-        </div>
-        <div class="text-2xl flex flex-wrap">
-          <p class="text-2xl text-center">Red social</p>
-          <a href="https://www.instagrm.com/n_kishin/" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/225px-Instagram_logo_2022.svg.png" alt="" width="30"> 
-          </a>
+    <section class="flex flex-col pt-2 pb-8 mx-auto max-w-7xl">
+        <h1  class ="text-4xl text-center font-bold pb-4" v-if="profile">Perfil Profesional: {{ profile.name }} {{ profile.lastname }}</h1>
+        <p class="text-2xl italic text-center pb-8"> {{ profile.summary }}</p>
+        <h2 class="text-2xl text-center font-semibold">Datos Personales</h2>
+        <div class="flex flex-row justify-center items-center">
+          <img :src="profile.url_pic" class="w-64 h-64 object-cover rounded-full">
+          <div class="pt-8">
+            <p class="bg-[#22092C] text-center text-[aliceblue] m-2.5 p-[15px] rounded-lg">Edad: {{profile.age}}</p>
+            <p class="bg-[#22092C] text-center text-[aliceblue] m-2.5 p-[15px] rounded-lg">Ciudad de origen: {{ profile.city }}</p>
+            <p class="bg-[#22092C] text-center text-[aliceblue] m-2.5 p-[15px] rounded-lg">Correo: {{ profile.email }}</p>
+          </div>
+          <div class="flex flex-col p-10">
+            <p class="text-2xl text-center pr-4 font-semibold">Red social:  </p>
+            <a :href="profile.instagram" target="_blank">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/225px-Instagram_logo_2022.svg.png" alt="" width="30"> 
+            </a>
+          </div>
         </div>
     </section>
-    <section class="flex w-1/2 p-4">
-        <h2  class="text-2xl font-bold mb-4">Intereses</h2>
-        <div v-if="profile" class="flex flex-wrap bg-[#872341] p-4 rounded-lg"> 
-          <ul>
-            <li class="list-inside p-2.5 bg-[#22092C] rounded-lg shadow" v-for="(hobbies, index) in profile.hobbies" :key="index">
-              <p class="font-semibold text-[aliceblue]">Nombre: {{ hobbies.name }}</p>
-              <p class="text-[aliceblue]">Descripción: {{ hobbies.description }}</p>
-            </li>
-          </ul>
-        </div>
-    
-    </section>
-    <section class="flex p-4 bg-[#22092C]">
-        <h2  class="text-2xl font-bold mb-4 text-[aliceblue]">Tabla de herramientas</h2>
-        <table class="w-full table-auto">
-          <thread class="bg-gray-800 text-white">
-            <tr>
-              <th class="px-4 py-2">Framework</th>
-              <th class="px-4 py-2">Nivel</th>
-              <th class="px-4 py-2">Año</th>
-            </tr>
-          </thread>
-          <tbody>
-            <tr v-for="(framework, index) in profile.frameworks" :key="index" class="text-center bg-gray-200">
-                <td class="border px-4 py-2">{{ framework.name}}</td>
-                <td class="borrder px-4 py-2">{{ framework.level }}</td>
-                <td>{{ framework.year}}</td>
-            </tr>
-          </tbody>
-        </table>
-    </section>
+      <section class="flex flex-col p-4 mx-auto max-w-7xl">
+          <h2  class="text-2xl font-bold mb-4 text-center">Intereses</h2>
+          <div v-if="profile" class="flex flex-wrap bg-[#872341] p-4 rounded-lg justify-center items-center"> 
+            <ul>
+              <li class="list-inside p-2.5 bg-[#22092C] rounded-lg shadow mb-4" v-for="(hobbies, index) in profile.hobbies" :key="index">
+                <p class="font-semibold text-[aliceblue]">Nombre: {{ hobbies.name }}</p>
+                <p class="text-[aliceblue]">Descripción: {{ hobbies.description }}</p>
+              </li>
+            </ul>
+          </div>
+      
+      </section>
+      <section class="flex flex-col p-4 bg-[#22092C] items-start pt-8 pb-16">
+          <h2  class="text-2xl font-bold mb-4 text-[aliceblue] mx-auto max-w-7xl">Tabla de herramientas</h2>
+          <table class="w-1/2 table-fixed mx-auto border-4 border-[#872341]">
+            <thead class="bg-[#872341] text-white">
+              <tr>
+                <th class="text-center">Framework</th>
+                <th class="text-center">Nivel</th>
+                <th class="text-center">Año</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(framework, index) in profile.frameworks" :key="index" class="text-center bg-white">
+                  <td class="border border-[#872341]">{{ framework.name}}</td>
+                  <td class="border border-[#872341]">{{ framework.level }}</td>
+                  <td class="border border-[#872341]">{{ framework.year}}</td>
+              </tr>
+            </tbody>
+          </table>
+      </section>
   </body>
   </html>
 </template>
